37471956102d1a7d6acd2af113aeb7f35e44fdbd
Craig Sturzaker <craig.sturzaker@arup.com>
2017-07-01 18:20:50 +0100

    added charts

--- src/app/talk-to-your-child/talk-to-your-child.module.ts
+++ src/app/talk-to-your-child/talk-to-your-child.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TalkToYourChildRoutingModule } from './talk-to-your-child-routing.module';
 import { TalkToYourChildComponent } from './talk-to-your-child.component';
+import { DrinkingChartComponent } from './drinking-chart/drinking-chart.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TalkToYourChildRoutingModule
   ],
-  declarations: [TalkToYourChildComponent]
+  declarations: [TalkToYourChildComponent, DrinkingChartComponent]
 })
 export class TalkToYourChildModule { }
