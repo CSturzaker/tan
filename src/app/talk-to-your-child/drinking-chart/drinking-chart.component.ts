bd0cba46e25eb2af8c069cc8fc49ef91e419e4c7
Craig Sturzaker <craig.sturzaker@arup.com>
2017-07-03 23:08:46 +0100

    added c3.js chart

--- src/app/talk-to-your-child/drinking-chart/drinking-chart.component.ts
+++ src/app/talk-to-your-child/drinking-chart/drinking-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import * as c3 from 'c3'
 
 @Component({
   selector: 'tan-drinking-chart',
@@ -13,124 +13,38 @@ export class DrinkingChartComponent implements OnInit {
 
   constructor() { }
 
-
   ngOnInit() {
-    this.labels = [
-      // tslint:disable-next-line:quotemark
-      ["Respondents whose", "parent \"don\'t mind them ", "drinking alcohol as long as", "I don\'t drink too much\""],
-      // tslint:disable-next-line:quotemark
-      ["Respondents whose", "parent don't like me", "drinking alcohol at all\""]
-    ]
     this.ctx = document.getElementById('drinkingChart');
-    this.chart = new Chart(this.ctx, {
-      type: 'horizontalBar',
-      data: {
-        labels: this.labels,
-        datasets: [{
-          label: 'How often do you drink alcohol?',
-          data: [79, 5],
-          backgroundColor: [
-            'rgba(237, 28, 36, 0.6)',
-            'rgba(237, 28, 36, 0.6)'
-          ],
-          borderColor: [
-            'rgba(237, 28, 36, 1)',
-            'rgba(237, 28, 36, 1)',
-          ],
-          borderWidth: 1
-        }]
+    this.chart = c3.generate({
+      bindto: this.ctx,
+      grid: {
+        x: {
+          show: false
+        },
+        y: {
+          show: true
+        }
       },
-      options: {
-        responsive: true,
-        tooltips: {
-            enabled: true,
-            mode: 'single',
-            callbacks: {
-                label: function(tooltipItems, data) {
-                    return tooltipItems.xLabel + '%';
-                }
-            }
+      data: {
+        columns: [
+          ['data1', 79, 5]
+        ],
+        types: {
+          data1: 'bar',
+        }
+      },
+      axis: {
+        rotated: true,
+        x: {
+          categories: ['Respondents whose \"parent don\'t mind them drinking alcohol as long as I dont drink too much\"',
+            'Respondents whose \"parent don\'t like me drinking alcohol at all\"'],
+          type: 'categorized',
         },
-        title: {
-            display: true,
-            fontColor: '#fff',
-            text: 'How often do you drink alcohol?'
-        },
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              drawBorder: false,
-              color: '#ed1c24',
-              display: true,
-              zeroLineColor: '#ed1c24'
-            },
-            ticks: {
-              min: 0,
-              max: 100,
-              fontColor: '#fff',
-              callback: function(label, index, labels) {
-                  return label + '%';
-              }
-            },
-
-          }],
-          yAxes: [{
-            type: 'category',
-            gridLines: {
-              display: false,
-              drawBorder: false
-            },
-            ticks: {
-              fontColor: '#fff'
-            }
-          }]
+        y: {
+          max: 90
         }
       }
+
     });
   }
-
-
-// var chart = c3.generate({
-//     padding: {
-//         right: 50
-//     },
-
-//     size: {
-//         height: 240,
-//         width: 480
-//     },
-//     grid: {
-//         x: {
-//             show: false
-//         },
-//         y: {
-//             show: true
-//         }
-//     },
-//     data: {
-//         columns: [
-//             ['data1', 79, 5]
-//         ],
-//         types: {
-//             data1: 'bar',
-//         }
-//     },
-//     axis: {
-//         rotated: true,
-//         x: {
-//             categories: ['Respondents whose \"parent don\'t mind them drinking alcohol as long as I dont drink too much\"',
-//             'Respondents whose \"parent don\'t like me drinking alcohol at all\"'],
-//             type: 'categorized',
-//           },
-//        y: {
-//            max: 90
-//        }
-//     }
-
-// });
-
-
 }
